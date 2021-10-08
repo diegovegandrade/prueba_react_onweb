dafd9a82eb2528a5de5fc6207ceeabc94c2a1213
dvega <dialvean@gmail.com>
2021-10-07 21:04:40 -0500

    cambio

---
+++ prueba_react/reac_example/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+       <h1>
+         React Application Dvega.
+       </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
